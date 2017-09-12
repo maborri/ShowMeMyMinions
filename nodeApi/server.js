9f833afb566e791ba5837c3b839496cb417cdd35
Borri <Matias_Borri@mcafee.com>
2017-09-12 17:50:06 -0300

    switch id api from callback to async and await

--- nodeApi/server.js
+++ nodeApi/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var request = require('request');
+var rp = require('request-promise');
 var cors = require('cors');
 
 var app = express();
@@ -8,35 +9,36 @@ const apiKey = "RGAPI-06b4d378-7f12-4a5c-9e20-644803d361c6";
 
 
 //API: get user ID:
-app.get('/getUserId/:region/:summoner', function (req, res, next) {
-  var region = req.params.region;
-  var summoner = req.params.summoner;
+async function getId(region, summoner) {
   var url = `https://${region}.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summoner}`;
-  var info = '';
   var options = {
     url: url,
     headers: {
       "X-Riot-Token": apiKey
     }
   };
-  request(options, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      info = JSON.parse(body);
-      console.log(info);   
-      res.send(info);
-    }
-    else{
-      console.log('error: ',error);
-    }
-  });
+  try {
+    var summonerInfo = await rp(options);  
+    return summonerInfo.accountId; 
+  }catch(err) {
+    console.log('Got an error:', err.message)
+  }
+}
+
+app.get('/getUserId/:region/:summoner', async function (req, res, next) {
+  var region = req.params.region;
+  var summoner = req.params.summoner;
+  var id = await getId(region, summoner);
+  console.log("id: ",id);
+  res.send(id);
 });
 
 //API get last 100 matches:
 app.get('/getLastMatches/:region/:id', function (req, res, next) {
     var id = req.params.id;
     var region = req.params.region;
-    var url = `https://${region}.api.riotgames.com/lol/match/v3/matchlists/by-account/${id}`;
-    var info = '';
+    var url = `https://${region}.api.riotgames.com/lol/match/v3/matchlists/by-account/${id}?beginIndex=0&endIndex=3`;
+    var idsArray = '';
     var options = {
       url: url,
       headers: {
@@ -45,9 +47,9 @@ app.get('/getLastMatches/:region/:id', function (req, res, next) {
     };
     request(options, (error, response, body) => {
       if (!error && response.statusCode == 200) {
-        info = JSON.parse(body);
-        console.log(info);   
-        res.send(info);
+        var info = JSON.parse(body);
+        idsArray = info.matches.map((match) => match.gameId);
+        res.send(info); 
       }
       else{
         console.log('error: ',error);
