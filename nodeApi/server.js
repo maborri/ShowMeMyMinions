42191dc0c6e154f58b564ac20e3c1b0c129dc2a7
maborri <matiasdborri@gmail.com>
2017-09-23 10:42:07 -0300

    server update

--- nodeApi/server.js
+++ nodeApi/server.js
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-var request = require('request');
-var express = require('express'),
-app = express(),
-port = process.env.PORT || 3000;
-var idinfo;
 
-var request = require('request');
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
-app.get('/getUserId', (request, response) => { 
-    request('https://la2.api.riotgames.com/lol/summoner/v3/summoners/by-name/dustz', (error, response, body) => {
-       response.json(body);
-    });
-    //"https://la2.api.riotgames.com/lol/summoner/v3/summoners/by-name/dustz"
-
- });
-
-app.listen(port);
-
-console.log('todo list RESTful API server started on: ' + port);
-=======
 var express = require('express');
 var request = require('request');
 var rp = require('request-promise');
